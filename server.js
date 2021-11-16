a432a7ff6e8a1cd2e0bfdfa49b40096465066b63
erfanshekari <75634046+erfanshekari@users.noreply.github.com>
2021-11-16 07:43:20 +0330

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const s3ReverseProxy = require("./s3ReverseProxy.js")
 
 const server = new s3ReverseProxy(5000, {
     region: 'default',
-    endpoint: 'https://s3.ir-thr-at1.arvanstorage.com'},
+    endpoint: 'endpoint_url'},
     function(headers) {
         // check user header for auth and return result as boolean
         return true
